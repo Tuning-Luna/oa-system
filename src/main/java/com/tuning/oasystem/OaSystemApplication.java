42e1c48a073e50f87c64585a55304afef4d8360d
Tuning-Luna <2081672492@qq.com>
2026-08-02 10:09:07 +0800

    add sout

--- src/main/java/com/tuning/oasystem/OaSystemApplication.java
+++ src/main/java/com/tuning/oasystem/OaSystemApplication.java
@@ -8,6 +8,9 @@ public class OaSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OaSystemApplication.class, args);
+		System.out.println("oa-system 启动成功");
+		System.out.println("http://localhost:8080");
+		System.out.println("http://localhost:8080/swagger-ui.html");
 	}
 
 }
